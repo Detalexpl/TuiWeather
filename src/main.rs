ff544493d90deb8aa1e34362e280266f3b2d6b36
detalex <frazeb09@gmail.com>
2026-02-28 15:03:28 +0100

    testing file downloading

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use crate::getting_weather::{get_url, get_weather, Location};
+use std::io;
+use std::fs::File;
+use std::io::Write;
 
 pub mod getting_weather;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::Client::new().get("https://raw.githubusercontent.com/Detalexpl/TuiWeather/refs/heads/master/worldcities.csv").send().await?.text().await?;
+    let mut file = File::create("path")?;
+    file.write_all(resp.as_bytes())?;
     let location = Location{
         latitude:52.22,
         longitude:21.01
