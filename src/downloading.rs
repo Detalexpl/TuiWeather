7f3bb52f62fb7cd4b92cee49b4126bc27c840c48
detalex <frazeb09@gmail.com>
2026-03-03 19:09:43 +0100

    0.1.4 repering for win 11

--- src/downloading.rs
+++ src/downloading.rs
@@ -2,7 +2,7 @@ use directories::ProjectDirs;
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
-use tokio::fs::create_dir;
+use tokio::fs::create_dir_all;
 
 //this function returns Path where dada shude be stored
 pub fn getting_path() -> Option<PathBuf> {
@@ -15,7 +15,7 @@ pub async fn downloading_data(path: PathBuf) -> Result<(), String> {
     let reqs = reqwest::Client::new().get("https://raw.githubusercontent.com/Detalexpl/TuiWeather/refs/heads/master/worldcities.csv")
         .send().await.map_err(|e| e.to_string())?.text().await.map_err(|e| e.to_string())?;
 
-    create_dir(&path).await.map_err(|e| e.to_string())?;
+    create_dir_all(&path).await.map_err(|e| e.to_string())?;
     let mut out = File::create(path.join("cities.csv")).map_err(|e| e.to_string())?;
     out.write_all(reqs.as_bytes()).map_err(|e| e.to_string())?;
 
